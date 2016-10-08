4ce820b6f06ea22de6b9b03c05754653924eca10
Alicia Miceli <aliciamiceli@Alicias-MacBook-Pro.local>
2016-10-08 10:12:54 -0500

    initial files

---
+++ javascripts/es6.js
@@ -0,0 +1,89 @@
+console.log('es6')
+
+//const
+const a=8;
+console.log("a", a);
+//a=12;
+//console.log("a", a);
+
+//let
+// let b=8;
+// console.log("b", b);
+// let b=12;
+// console.log("b", b);
+// //arrow functions
+// var sum = function() {
+//   let a=1, b=2;
+//   return a+b;
+// }
+// console.log("hardcoded sum in ES5", sum());
+
+// var sum2 =() => {
+//   let a=1, b=2;
+//   return a+b;
+// }
+
+// console.log("es6", sum2());
+
+var realsum2 = (num1, num2) => {
+  return num1+num2;
+}
+
+console.log("realsum ES6", realsum2(5, 4));
+//string templates
+let m = "biteme"
+console.log("m", m);
+let r = `hello my name is ${m}`
+console.log(r);
+
+
+//object literal property shorthand
+let wow="Hi there", es6="ES6", numNum=() =>{console.log("howdy")}
+
+let myOldObject = {
+  wow:wow,
+  es6:es6,
+  numNum:numNum
+}
+
+console.log("myOldObject", myOldObject );
+
+let myNewObject = {
+  wow, es6, numNum
+}
+
+console.log("myNewObject", myNewObject);
+
+
+//method properties
+var e5Obj = {
+  foo: function(){console.log("foo")},
+  bar: function(){console.log("foo")}
+}
+
+console.log("e5Obj", e5Obj);
+
+var es6Obj = {
+  foo(){console.log('foo')},
+  bar(){console.log("bar");}
+}
+
+console.log("es6Obj", es6Obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
